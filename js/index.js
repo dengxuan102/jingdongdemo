aa00c5bfef2aebdc587afa1f36b6221c93f0aff0
dengxuan102 <d474764333@163.com>
2019-05-02 23:43:28 +0800

    ""

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
-function hidden_img() {
-    document.getElementsByClassName("hidden_img").style.display = "none";
-}
+var mySwiper = new Swiper(".swiper-container1", {
+  direction: "horizontal",
+  // height: 300,
+  // width: 600,
+  loop: true,
+  autoplay: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+    // hideOnClick: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+
+var mySwiper2 = new Swiper(".swiper-container2", {
+  slidesPerViews: 4,
+  direction: "horizontal",
+  spaceBewteen: 10,
+  slidesPerGroup: 5,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
